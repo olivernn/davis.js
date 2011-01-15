d8fdef76d966cda04866faf742e885c4e022f91a
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-15 17:04:28 +0000

    extract query params form get path

--- src/davis.request.js
+++ src/davis.request.js
@@ -1,10 +1,21 @@
 Davis.Request = function (event) {
   var self = this;
   var target = event.target;
+  var fullPath = $(event.target).attr('href')
 
-  this.path = $(event.target).attr('href')
   this.method = 'get'
   this.params = {}
+
+  var queryParams = fullPath.split("?")[1];
+
+  if (queryParams) {
+    queryParams.split("&").forEach(function (keyval) {
+      self.params[keyval.split("=")[0]] = keyval.split("=")[1]
+    })
+  };
+
+  this.path = fullPath.replace(/\?.+/, "");
+
   // var clickRequest = function () {
   //   self.path = target.href.split(/\?/)[0];
   //   self.method = 'get';
