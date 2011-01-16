df5907daa20d35f1994a84ebd72fd7d605d99de2
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-16 19:29:10 +0000

    refactor interceptor

--- src/davis.interceptor.js
+++ src/davis.interceptor.js
@@ -2,33 +2,31 @@ Davis.Interceptor = (function () {
 
   var links
 
-  var bindAllLinks = function () {
-    $(document).delegate('a', 'click', function (event) {
-      var target = $(event.target);
-      var request = new Davis.Request ({
-        method: 'get',
-        fullPath: target.attr('href')
-      });
+  var handler = function (targetExtractor) {
+    return function (event) {
+      var request = new Davis.Request (targetExtractor.call($(event.target)));
       Davis.History.pushState(request, "asfd", request.path)
       return false;
-    })
-  }
+    };
+  };
 
-  var bindAllForms = function () {
-    $(document).delegate('form', 'submit', function (event) {
-      var target = $(event.target);
-      var request = new Davis.Request ({
-        method: target.attr('method'),
-        fullPath: [target.attr('action'), "?", target.serialize()].join("")
-      });
-      Davis.History.pushState(request, "form", request.path)
-      return false;
-    })
-  }
+  var clickHandler = handler(function () {
+    return {
+      method: 'get',
+      fullPath: this.attr('href')
+    };
+  });
+
+  var submitHandler = handler(function () {
+    return {
+      method: this.attr('method'),
+      fullPath: [this.attr('action'), "?", this.serialize()].join("")
+    };
+  });
 
   var enable = function () {
-    bindAllForms();
-    bindAllLinks();
+    $(document).delegate('form', 'submit', submitHandler)
+    $(document).delegate('a', 'click', clickHandler)
   }
 
   return {
