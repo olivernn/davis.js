5680681a2b4c273176d41a790b0d342d0e98bcba
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-17 21:27:02 +0000

    bundling

---
+++ lib/bundler.rb
@@ -0,0 +1,59 @@
+require 'fileutils'
+
+require 'rubygems'
+require 'closure-compiler'
+require 'fewer'
+
+class Bundler
+  DIST_DIR = File.expand_path('../../dist', __FILE__)
+  SRC_DIR = File.expand_path('../../src', __FILE__)
+
+  class << self
+    def bundle!
+      FileUtils.mkdir_p(DIST_DIR)
+
+      write "#{DIST_DIR}/davis-#{version}.js" do
+        Fewer::Engines::Js.new(SRC_DIR, files).read
+      end
+
+      write "#{DIST_DIR}/davis-#{version}.min.js" do
+        Fewer::Engines::Js.new(SRC_DIR, files, :min => true).read
+      end
+    end
+
+    def bundled
+      Fewer::Engines::Js.new(SRC_DIR, files).read
+    end
+
+    private
+      def files
+        @files ||= %w(
+          davis
+          davis.listener
+          davis.event
+          davis.logger
+          davis.route
+          davis.history
+          davis.request
+          davis.app
+        )
+      end
+
+      def header
+        @header ||= File.read(File.join(SRC_DIR, 'header.js'))
+      end
+
+      def version
+        @version ||= File.read('VERSION').strip
+      end
+
+      def write(path, &block)
+        puts "Generating #{path}"
+
+        File.open(path, 'w') do |f|
+          f.write header.gsub('@VERSION', version)
+          f.write yield.gsub('@VERSION', version)
+        end
+      end
+  end
+end
---
+++ src/header.js
@@ -0,0 +1,5 @@
+// davis.js JavaScript Routing, version: @VERSION
+// (c) 2011 Oliver Nightingale
+//
+//  Released under MIT license.
+//
