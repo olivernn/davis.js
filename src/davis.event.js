6512dbe3b7214f76112964c5a1e595a99713337d
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-19 21:50:41 +0000

    event simplified

--- src/davis.event.js
+++ src/davis.event.js
@@ -1,24 +1,44 @@
-Davis.event = (function () {
+// Davis.event = (function () {
+// 
+//   var callbacks = {}
+// 
+//   var bind = function (eventName, callback) {
+//     if (!callbacks[eventName]) callbacks[eventName] = [];
+//     callbacks[eventName].push(callback);
+//     return this;
+//   }
+// 
+//   var trigger = function (eventName, data) {
+//     var self = this;
+//     if (!callbacks[eventName]) callbacks[eventName] = [];
+//     callbacks[eventName].forEach(function (callback) {
+//       callback.call(self, data);
+//     }) 
+//     return this;
+//   }
+// 
+//   return {
+//   
+//     bind: bind,
+//     trigger: trigger
+//   }
+// })()
 
-  var callbacks = {}
+Davis.event = {
+  _callbacks: {},
 
-  var bind = function (eventName, callback) {
-    if (!callbacks[eventName]) callbacks[eventName] = [];
-    callbacks[eventName].push(callback);
+  bind: function (eventName, callback) {
+    if (!this._callbacks[eventName]) this._callbacks[eventName] = [];
+    this._callbacks[eventName].push(callback);
     return this;
-  }
+  },
 
-  var trigger = function (eventName, data) {
+  trigger: function (eventName, data) {
     var self = this;
-    if (!callbacks[eventName]) callbacks[eventName] = [];
-    callbacks[eventName].forEach(function (callback) {
+    if (!this._callbacks[eventName]) this._callbacks[eventName] = [];
+    this._callbacks[eventName].forEach(function (callback) {
       callback.call(self, data);
     }) 
     return this;
   }
-
-  return {
-    bind: bind,
-    trigger: trigger
-  }
-})()
+}
