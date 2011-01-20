2e57cf420336512800eef276f54a306944bb7366
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-20 09:55:54 +0000

    simplify event module

--- src/davis.event.js
+++ src/davis.event.js
@@ -1,29 +1,3 @@
-// Davis.event = (function () {
-// 
-//   var callbacks = {}
-// 
-//   var bind = function (eventName, callback) {
-//     if (!callbacks[eventName]) callbacks[eventName] = [];
-//     callbacks[eventName].push(callback);
-//     return this;
-//   }
-// 
-//   var trigger = function (eventName, data) {
-//     var self = this;
-//     if (!callbacks[eventName]) callbacks[eventName] = [];
-//     callbacks[eventName].forEach(function (callback) {
-//       callback.call(self, data);
-//     }) 
-//     return this;
-//   }
-// 
-//   return {
-//   
-//     bind: bind,
-//     trigger: trigger
-//   }
-// })()
-
 Davis.event = {
   _callbacks: {},
 
@@ -41,4 +15,5 @@ Davis.event = {
     }) 
     return this;
   }
-}
+};
+
