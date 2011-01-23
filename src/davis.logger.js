64d253e2fcfa13b36992d8de4d06b4041cb0cfb1
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-23 18:21:31 +0000

    docs for logger module

--- src/davis.logger.js
+++ src/davis.logger.js
@@ -1,27 +1,68 @@
+/*!
+ * Davis - logger
+ * Copyright (C) 2011 Oliver Nightingale
+ * MIT Licensed
+ */
+
+/**
+ * A module for enhancing the standard logging available through the console object.
+ * Used internally in Davis and available for use outside of Davis.
+ *
+ * Generates log messages of varying severity in the format:
+ *
+ * `[Sun Jan 23 2011 16:15:21 GMT+0000 (GMT)] <message>`
+ */
 Davis.logger = (function () {
 
+  /**
+   * Generating the timestamp portion of the log message
+   * @private
+   */
   var timestamp = function (){
     return "[" + Date() + "]";
   }
 
+  /**
+   * Pushing the timestamp onto the front of the arguments to log
+   * @private
+   */
   var prepArgs = function (args) {
     var a = Array.prototype.slice.call(args)
     a.unshift(timestamp())
     return a
   }
 
+  /**
+   * Prints an error message to the console if the console is available.
+   *
+   * @params {String} All arguments are combined and logged to the console.
+   */
   var error = function () {
     if (window.console) console.error.apply(console, prepArgs(arguments))
   }
 
+  /**
+   * Prints an info message to the console if the console is available.
+   *
+   * @params {String} All arguments are combined and logged to the console.
+   */
   var info = function () {
     if (window.console) console.info.apply(console, prepArgs(arguments))
   }
 
+  /**
+   * Prints a warning message to the console if the console is available.
+   *
+   * @params {String} All arguments are combined and logged to the console.
+   */
   var warn = function () {
     if (window.console) console.warn.apply(console, prepArgs(arguments))
   }
 
+  /**
+   * Exposes the public methods of the module
+   * @private
+   */
   return {
     error: error,
     info: info,
