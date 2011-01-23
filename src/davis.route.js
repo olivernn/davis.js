263498a3129cf850a60074a9f533c53f97f826d2
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-23 21:33:00 +0000

    docs for davis.route

--- src/davis.route.js
+++ src/davis.route.js
@@ -1,9 +1,42 @@
+/*!
+ * Davis - Route
+ * Copyright (C) 2011 Oliver Nightingale
+ * MIT Licensed
+ */
+
+/**
+ * Scope used whilst creating the Davis.Route constructor
+ * @private
+ */
 Davis.Route = (function () {
 
+/**
+ * RegExps for extracting path params
+ * @private
+ */
   var pathNameRegex = /:([\w\d]+)/g;
   var pathNameReplacement = "([^\/]+)";
 
-  var klass = function (method, path, callback) {
+/**
+ * Davis.Routes are the main part of a Davis application.  They consist of a HTTP method, a path
+ * and a callback function.  When a link or a form that Davis has bound to are clicked or submitted
+ * a request is pushed on the history stack and a route that matches the path and method of the
+ * generated request is run.
+ *
+ * Inside the callback function 'this' is bound to the request.
+ *
+ * @constructor
+ * @param {String} method - This should be one of either 'get', 'post', 'put' or 'delete'
+ * @param {String} path - This string can contain place holders for variables, e.g. '/user/:id'
+ * @param {Function} callback - A callback that will be called when a request matching both the path and method is triggered.
+ *
+ * Example:
+ *     var route = new Davis.Route ('get', '/foo/:id', function (req) {
+ *       var id = req.params['id']
+ *       // do something interesting!
+ *     })
+ */
+  var Route = function (method, path, callback) {
     var convertPathToRegExp = function () {
       if (!(path instanceof RegExp)) {
         return new RegExp("^" + path.replace(pathNameRegex, pathNameReplacement) + "$", "g");
@@ -32,12 +65,29 @@ Davis.Route = (function () {
     this.callback = callback;
   }
 
-  klass.prototype = {
+  Route.prototype = {
 
+    /**
+     * ## route.match('get', '/foo')
+     * Tests whether or not a route matches a particular request.
+     *
+     * @param {String} method
+     * @param {String} path
+     * @returns {Boolean}
+     */
     match: function (method, path) {
       return (this.method.test(method)) && (this.path.test(path))
     },
 
+    /**
+     * ## route.run(request)
+     * Runs the callback associated with a particular route against the passed request.
+     * Any named params in the request path are extracted, as per the routes path, and
+     * added onto the requests params object.
+     *
+     * @params {Davis.Request} request
+     * @returns {Object} whatever the routes callback returns
+     */
     run: function (request) {
       this.path.lastIndex = 0
       var matches = this.path.exec(request.path);
@@ -51,10 +101,21 @@ Davis.Route = (function () {
       return this.callback.call(request, request);
     },
 
+    /**
+     * ## route.toString()
+     * Converts the route to a string representation of itself by combining the method and path
+     * attributes.
+     *
+     * @returns {String} string representation of the route
+     */
     toString: function () {
       return [this.method, this.path].join(' ');
     }
   };
 
-  return klass;
+  /**
+   * exposing the constructor
+   * @private
+   */
+  return Route;
 })()
