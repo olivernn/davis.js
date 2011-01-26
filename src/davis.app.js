97d75f49ae2fd1eb254a0285e4bf55fa298157fb
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-26 14:36:53 +0000

    re-arrange start method and generate a request for the initial page load

--- src/davis.app.js
+++ src/davis.app.js
@@ -83,6 +83,37 @@ Davis.App = (function () {
     start: function () {
       var self = this;
 
+      var runFilterWith = function (request) {
+        return function (filter) {
+          var result = filter.run(request, request);
+          return (typeof result === "undefined" || result);
+        }
+      }
+
+      var beforeFiltersPass = function (request) {
+        return self.lookupBeforeFilter(request.method, request.path)
+                      .every(runFilterWith(request))
+      }
+
+      var handleRequest = function (request) {
+        if (beforeFiltersPass(request)) {
+          self.trigger('lookupRoute', request)
+          var route = self.lookupRoute(request.method, request.path);
+          if (route) {
+            self.trigger('runRoute', request, route);
+            route.run(request);
+            self.lookupAfterFilter(request.method, request.path)
+                  .every(runFilterWith(request));
+          } else {
+            self.trigger('routeNotFound', request);
+          }
+        } else {
+          self.trigger('requestHalted', request)
+        }
+      }
+
+      Davis.history.onChange(handleRequest);
+
       this
         .bind('runRoute', function (request) {
           self.settings.logger.info("runRoute: " + request.toString());
@@ -96,37 +127,10 @@ Davis.App = (function () {
 
       this.listen();
       this.trigger('start')
-
-      var runFilterWith = function (request) {
-        return function (filter) {
-          var result = filter.run(request, request);
-          return (typeof result === "undefined" || result);
-        }
-      }
-
-      var beforeFiltersPass = function (request) {
-        return self.lookupBeforeFilter(request.method, request.path)
-                      .every(runFilterWith(request))
-      }
-
-      Davis.history.onChange(function (request) {
-        if (beforeFiltersPass(request)) {
-          self.trigger('lookupRoute', request)
-          var route = self.lookupRoute(request.method, request.path);
-          if (route) {
-            self.trigger('runRoute', request);
-            route.run(request);
-            self.lookupAfterFilter(request.method, request.path)
-                  .every(runFilterWith(request));
-          } else {
-            self.trigger('routeNotFound', request);
-          }
-        } else {
-          self.trigger('requestHalted', request)
-        }
-      });
-
       this.running = true;
+
+      handleRequest(Davis.Request.forPageLoad())
+
     },
 
     /**
