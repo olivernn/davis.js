cf56eb98240e3d6da1eeaed26c7c305de340e448
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-02-10 13:15:21 +0000

    Add method to check whether pushState is available

--- src/davis.js
+++ src/davis.js
@@ -1,5 +1,28 @@
-Davis = function (routes) {
+/*!
+ * Davis
+ * Copyright (C) 2011 Oliver Nightingale
+ * MIT Licensed
+ */
+
+/**
+ * Convinience method for instantiating a new Davis app and configuring it to use the passed
+ * routes and subscriptions.
+ *
+ * @param {Function} config A function that will be run with a newly created Davis.App as its context,
+ * should be used to set up app routes, subscriptions and settings etc.
+ */
+Davis = function (confg) {
   var app = new Davis.App ();
-  routes.call(app);
+  config.call(app);
   return app
 };
+
+/**
+ * ## Davis.supported()
+ * Checks whether Davis is supported in the current browser
+ *
+ * @returns {Boolean}
+ */
+Davis.supported = function () {
+  return (typeof window.history.pushState == 'function')
+}
