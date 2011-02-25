d6cdca285488f13ec88064f983bcb4fabc818a70
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-02-25 09:01:03 +0000

    documentation for Davis.googleAnalytics plugin

--- src/plugins/davis.google_analytics.js
+++ src/plugins/davis.google_analytics.js
@@ -1,16 +1,51 @@
+/*!
+ * Davis - googleAnalytics
+ * Copyright (C) 2011 Oliver Nightingale
+ * MIT Licensed
+ */
+
+/**
+ * Davis.googleAnalytics is a plugin to track Davis requests in google analytics.  It automatically
+ * tracks every GET request and adds helpers to manually track other requests and to prevent a
+ * particular request from being tracked.
+ *
+ * To include this plugin in your application first include a script tag for it and then in your
+ * app do the following.
+ *
+ *    this.use(Davis.googleAnalytics)
+ *
+ * @plugin
+ */
 Davis.googleAnalytics = function () {
+
+  /**
+   * whether a route should be tracked or not
+   * @private
+   */
   var shouldTrack = true
 
+  /**
+   * bind to the apps routeComplete event and track the request unless explicitly told not to.
+   * @private
+   */
   this.bind('routeComplete', function (req) {
     if (shouldTrack) req.track()
     shouldTrack = true
   })
 
   this.helpers({
+    /**
+     * ## request.noTrack
+     * Disable tracking for this request
+     */
     noTrack: function () {
       shouldTrack = false
     },
 
+    /**
+     * ## request.track
+     * Track this request in google analytics
+     */
     track: function () {
       if (_gaq && this.method == 'get') _gaq.push(['_trackPageview', this.path])
     }
