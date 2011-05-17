5375bf0ee7a5841932c3917c9eade46474c86730
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-05-17 10:26:19 +0100

    Davis.toArray has been moved into Davis.utils

--- src/davis.js
+++ src/davis.js
@@ -35,17 +35,3 @@ Davis.supported = function () {
  * @returns {Function}
  */
 Davis.noop = function () {}
-
-/**
- * ## Davis.toArray
- * A convinience function for converting arguments to a proper array
- *
- * @private
- * @param {args} a functions arguments
- * @param {start} an integer at which to start converting the arguments to an array
- * @returns {Array}
- */
-Davis.toArray = function (args, start) {
-  var start = start || 0
-  return Array.prototype.slice.call(args, start)
-}
