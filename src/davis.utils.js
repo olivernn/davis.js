72efd1ba986556c1067a07cfe23218ac5fed2471
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-05-17 10:25:48 +0100

    add basic documentation for Davis.utils

--- src/davis.utils.js
+++ src/davis.utils.js
@@ -1,5 +1,20 @@
+/*!
+ * Davis - utils
+ * Copyright (C) 2011 Oliver Nightingale
+ * MIT Licensed
+ */
+
+/**
+ * A module that provides wrappers around modern JavaScript so that native implementations are used
+ * whereever possible and JavaScript implementations are used in those browsers that do not natively
+ * support them.
+ */
 Davis.utils = (function () {
 
+  /**
+   * Checks the arguments for type errors
+   * @private
+   */
   var checkForTypeError = function (array, fn) {
     if (array === void 0 || array === null) throw new TypeError();
     var t = Object(array);
@@ -7,6 +22,18 @@ Davis.utils = (function () {
     if (typeof fun !== "function") throw new TypeError();
   }
 
+  /**
+   * ## Davis.utils.every
+   * A wrapper around native Array.prototype.every that falls back to a pure JavaScript implementation
+   * in browsers that do not support Array.prototype.every.  For more details see the full docs on MDC
+   * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/every
+   *
+   * @private
+   * @param {array} the array to loop through
+   * @param {fn} the function to that performs the every check
+   * @param {thisp} an optional param that will be set as fn's this value
+   * @returns {Array}
+   */
   if (Array.prototype.every) {
     var every = function (array, fn /*, thisp */) {
       return array.every(fn, arguments[2])
@@ -24,6 +51,18 @@ Davis.utils = (function () {
     }
   };
 
+  /**
+   * ## Davis.utils.forEach
+   * A wrapper around native Array.prototype.forEach that falls back to a pure JavaScript implementation
+   * in browsers that do not support Array.prototype.forEach.  For more details see the full docs on MDC
+   * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
+   *
+   * @private
+   * @param {array} the array to loop through
+   * @param {fn} the function to apply to every element of the array
+   * @param {thisp} an optional param that will be set as fn's this value
+   * @returns {Array}
+   */
   if (Array.prototype.forEach) {
     var forEach = function (array, fn /*, thisp */) {
       return array.forEach(fn, arguments[2])
@@ -39,6 +78,18 @@ Davis.utils = (function () {
     };
   };
 
+  /**
+   * ## Davis.utils.filter
+   * A wrapper around native Array.prototype.filter that falls back to a pure JavaScript implementation
+   * in browsers that do not support Array.prototype.filter.  For more details see the full docs on MDC
+   * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/filter
+   *
+   * @private
+   * @param {array} the array to filter
+   * @param {fn} the function to do the filtering
+   * @param {thisp} an optional param that will be set as fn's this value
+   * @returns {Array}
+   */
   if (Array.prototype.filter) {
     var filter = function (array, fn /*, thisp */) {
       return array.filter(fn, arguments[2])
@@ -60,6 +111,19 @@ Davis.utils = (function () {
     };
   };
 
+
+  /**
+   * ## Davis.utils.map
+   * A wrapper around native Array.prototype.map that falls back to a pure JavaScript implementation
+   * in browsers that do not support Array.prototype.map.  For more details see the full docs on MDC
+   * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/map
+   *
+   * @private
+   * @param {array} the array to map through
+   * @param {fn} the function to do the mapping
+   * @param {thisp} an optional param that will be set as fn's this value
+   * @returns {Array}
+   */
   if (Array.prototype.map) {
     var map = function (array, fn /*, thisp */) {
       return array.map(fn, arguments[2])
@@ -78,11 +142,24 @@ Davis.utils = (function () {
     };
   };
 
+  /**
+   * ## Davis.utils.toArray
+   * A convinience function for converting arguments to a proper array
+   *
+   * @private
+   * @param {args} a functions arguments
+   * @param {start} an integer at which to start converting the arguments to an array
+   * @returns {Array}
+   */
   var toArray = function (args, start) {
     var start = start || 0
     return Array.prototype.slice.call(args, start)
   }
 
+  /**
+   * Exposing the public interface to the Utils module
+   * @private
+   */
   return {
     every: every,
     forEach: forEach,
