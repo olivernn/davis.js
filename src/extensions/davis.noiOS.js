e20f5de04c8217629457e81c035ac56940c4a2b2
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-09-06 18:11:29 +0100

    add noIOS extension for disabling support in iOS browsers

---
+++ src/extensions/davis.noiOS.js
@@ -0,0 +1,27 @@
+/*!
+ * Davis - no_iOS
+ * Copyright (C) 2011 Oliver Nightingale
+ * MIT Licensed
+ */
+
+/**
+ * Davis.noIOS is a plugin that will mark iOS devices of version less than 4 as unsupported.
+ * iOS devices have several bugs in their implementation of pushState, if these bugs are causing
+ * issues for your application you can use this extension to disable support for iOS devices.
+ *
+ *    this.extend(Davis.noIOS)
+ *
+ * @plugin
+ */
+Davis.noIOS = function () {
+
+  Davis.supported = (function (uber) {
+    var iosRegex = /\([iPhone;]?[iPad;]?[iPod;]?.+OS (\d)_(\d)/,
+        match = iosRegex.exec(window.navigator.userAgent),
+        majorVersionNumber = match && match[1] || Infinity;
+
+    return function () {
+      return uber() && (!match && majorVersionNumber > 4)
+    }
+  })(Davis.supported)
+}
