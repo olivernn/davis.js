4609e4fe0132b3daa7a3419c293869031026a364
Tj Holowaychuk <tj@vision-media.ca>
2011-10-12 00:28:03 -0700

    Refactored App/events

--- src/davis.event.js
+++ src/davis.event.js
@@ -4,53 +4,55 @@
  * MIT Licensed
  */
 
-/**
- * Slice reference.
- */
+Davis.event = (function(){
 
-var slice = [].slice;
+  /**
+   * Slice reference.
+   */
 
-/**
- * EventEmitter.
- */
+  var slice = [].slice;
 
-Davis.Emitter = function Emitter() {
-  this.callbacks = {};
-};
+  /**
+   * Listen on the given `event` with `fn`.
+   *
+   * @param {String} event
+   * @param {Function} fn
+   */
 
-/**
- * Listen on the given `event` with `fn`.
- *
- * @param {String} event
- * @param {Function} fn
- */
+  function bind(event, fn){
+    this.callbacks = this.callbacks || {};
+    (this.callbacks[event] = this.callbacks[event] || [])
+      .push(fn);
+    return this;
+  };
 
-Davis.Emitter.prototype.on =
-Davis.Emitter.prototype.bind = function(event, fn){
-  (this.callbacks[event] = this.callbacks[event] || [])
-    .push(fn);
-  return this;
-};
+  /**
+   * Emit `event` with the given args.
+   *
+   * @param {String} event
+   * @param {Mixed} ...
+   */
 
-/**
- * Emit `event` with the given args.
- *
- * @param {String} event
- * @param {Mixed} ...
- */
+  function trigger(event){
+    this.callbacks = this.callbacks || {};
+    var args = slice.call(arguments, 1)
+      , callbacks = this.callbacks[event];
 
-Davis.Emitter.prototype.emit =
-Davis.Emitter.prototype.trigger = function(event){
-  var args = slice.call(arguments, 1)
-    , callbacks = this.callbacks[event];
-
-  if (callbacks) {
-    for (var i = 0, len = callbacks.length; i < len; ++i) {
-      callbacks[i].apply(this, args)
+    if (callbacks) {
+      for (var i = 0, len = callbacks.length; i < len; ++i) {
+        callbacks[i].apply(this, args)
+      }
     }
-  }
 
-  return this;
-};
+    return this;
+  };
 
+  return {
+      on: bind
+    , bind: bind
+    , trigger: trigger
+    , emit: trigger
+  };
+
+})();
 
