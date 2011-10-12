19e55a829e2f3af59451b708864b9a48ff361f22
Tj Holowaychuk <tj@vision-media.ca>
2011-10-11 23:51:42 -0700

    Revert "Removed "every" util that is not used"
    This reverts commit 03006fb7d8d1f42db3bcebdda75d062a67794e46.

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -1,5 +1,63 @@
 module("Davis.utils")
 
+test("every where every element passes the test", function () {
+  var a = [1,2,3]
+  var result = Davis.utils.every(a, function (element) {
+    return element < 10
+  })
+
+  ok(result, "every element of the array is less than 10")
+})
+
+test("every where one of the items doesn't pass the test", function () {
+  var a = [1,2,3]
+  var result = Davis.utils.every(a, function (element) {
+    return element < 2
+  })
+
+  ok(!result, "not every element of the array is less than 2")
+})
+
+test("every should only call the callback whilst the test is passing", function () {
+  var a = [1,2,3]
+  var count = 0
+  var result = Davis.utils.every(a, function (element) {
+    count++
+    return element < 2
+  })
+
+  ok(!result, "not every element of the array is less than 2")
+  equal(2, count, "should stop iterating as soon as the test is false")
+})
+
+test("every arguments passed to the function", function () {
+  var a = ["foo"],
+      b, element, index
+
+  Davis.utils.every(a, function (e, i, array) {
+    b = array
+    element = e
+    index = i
+    return true
+  })
+
+  equal(element, "foo", "should yield the element of the array as the first argument")
+  equal(index, 0, "should yield the index of the item in the array as the second argument")
+  same(b, a, "should yeild the array being iterated as the third argument")
+})
+
+test("every setting the context of the callback function", function () {
+  var a = ["foo"],
+      context = {},
+      self
+
+  Davis.utils.every(a, function () {
+    self = this
+  }, context)
+
+  same(self, context, "should be able to set the context of the function")
+})
+
 test("forEach iterating over an array", function () {
   var a = [1,2,3],
       b = [],
