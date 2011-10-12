643356c379f3eceef86e5cc4478f22d2779adf0e
Tj Holowaychuk <tj@vision-media.ca>
2011-10-12 00:19:15 -0700

    Added Davis.Emitter

--- src/davis.event.js
+++ src/davis.event.js
@@ -5,75 +5,52 @@
  */
 
 /**
- * A module that can be mixed into any object to provide basic event functionality.
- * This module is mixed into the prototype of Davis.App.
+ * Slice reference.
  */
-Davis.event = {
 
-  /**
-   * an object used for storing event callbacks
-   * @private
-   */
-  _callbacks: {},
+var slice = [].slice;
 
-  /**
-   * ## app.bind
-   * Binds a callback to a named event.
-   *
-   * @param {String} eventName
-   * @param {Function} callback
-   *
-   * The callback will be called with its context set to which ever object this module
-   * has been mixed into.  Any data that is passed when triggering the event will be passed
-   * to the callback function as the first parameter.
-   *
-   * The following events are triggered internally by Davis and can be bound to:
-   *
-   *  __start__ : Triggered when the application is started
-   *
-   *  __lookupRoute__ : Triggered before looking up a route, the request being looked up is passed as an argument
-   *
-   *  __runRoute__ : Triggered before running a route, the request and route being run are passed as arguments
-   *
-   *  __routeNotFound__ : Triggered if no route for the current request can be found, the current request is passed as an arugment
-   *
-   *  __requestHalted__ : Triggered when a before filter halts the current request, the current request is passed as an argument
-   *
-   *  __unsupported__ : Triggered when starting a Davis app in a browser that doesn't support html5 pushState
-   *
-   * ### Example:
-   *     app.bind('runRoute', function () {
-   *       console.log('just about to run a route!')
-   *     })
-   */
-  bind: function (eventName, callback) {
-    if (!this._callbacks[eventName]) this._callbacks[eventName] = [];
-    this._callbacks[eventName].push(callback);
-    return this;
-  },
-  /**
-   * ## app.trigger
-   * Triggers an event on the current object.
-   *
-   * @param {String} eventName
-   * @param {Object} data (optional)
-   *
-   * Triggers an event on the current object, all callbacks bound using bind will be called.
-   * An optional second param can be passed which will be passed as an argument to each callback
-   * bound to the named event.
-   *
-   * ### Example:
-   *     app.triger('foo')
-   *     app.trigger('bar', {baz: true})
-   */
-  trigger: function (eventName) {
-    var self = this;
-    var args = arguments;
-    if (!this._callbacks[eventName]) this._callbacks[eventName] = [];
-    Davis.utils.forEach(this._callbacks[eventName], function (callback) {
-      callback.apply(self, Davis.utils.toArray(args, 1));
-    }) 
-    return this;
-  }
+/**
+ * EventEmitter.
+ */
+
+Davis.Emitter = function Emitter() {
+  this.callbacks = {};
 };
+
+/**
+ * Listen on the given `event` with `fn`.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ */
+
+Davis.Emitter.prototype.on =
+Davis.Emitter.prototype.bind = function(event, fn){
+  (this.callbacks[event] = this.callbacks[event] || [])
+    .push(fn);
+  return this;
+};
+
+/**
+ * Emit `event` with the given args.
+ *
+ * @param {String} event
+ * @param {Mixed} ...
+ */
+
+Davis.Emitter.prototype.emit =
+Davis.Emitter.prototype.trigger = function(event){
+  var args = slice.call(arguments, 1)
+    , callbacks = this.callbacks[event];
+
+  if (callbacks) {
+    for (var i = 0, len = callbacks.length; i < len; ++i) {
+      callbacks[i].apply(this, args)
+    }
+  }
+
+  return this;
+};
+
 
