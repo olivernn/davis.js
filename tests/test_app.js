f0ca5558564b8b8c18374c9d35e5de10f0f7ba40
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-11-04 16:46:02 +0000

    get tests passing individually in IE8

--- tests/test_app.js
+++ tests/test_app.js
@@ -1,20 +1,24 @@
 module("Davis.App")
 
 test('Davis() fn should be optional', function(){
+  Davis.extend(Davis.hashRouting())
   ok(Davis() instanceof Davis.App, 'Davis() should return an app');
 });
 
 test("should auto start the application", function () {
+  Davis.extend(Davis.hashRouting())
   var app = Davis()
   ok(app.running, 'Davis() should auto start the app')
 })
 
 test("can only start an app that is stopped", function () {
+  Davis.extend(Davis.hashRouting())
   var startCount = 0,
-      app = new Davis.App ()
-      app.bind('start', function () {
-        startCount++
-      })
+      app = factory('app');
+
+  app.bind('start', function () {
+    startCount++
+  })
 
   app.start()
   app.start()
