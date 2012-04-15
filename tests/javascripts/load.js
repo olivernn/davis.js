7eaed22aab3fe555f8f8633f35629334c4f6fc60
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-04-15 13:05:02 +0100

    custom test loader

---
+++ tests/javascripts/load.js
@@ -0,0 +1,28 @@
+;(function () {
+
+  var global = this
+
+  var loadScript = function (src) {
+    var script = document.createElement('script')
+    script.src = src
+    document.head.appendChild(script)
+  }
+
+  var load = function () {
+    var scripts = Array.prototype.slice.call(arguments)
+        condition = scripts[scripts.length - 1]
+
+    if (typeof(condition) === "string") {
+      condition = function () { return true }
+    } else {
+      scripts.pop()
+    }
+
+    for (var i=0; i < scripts.length; i++) {
+      if (condition()) loadScript(scripts[i])
+    };
+  }
+
+  global.load = load
+
+})()
